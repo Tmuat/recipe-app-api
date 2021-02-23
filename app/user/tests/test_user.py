826b50b5a735d352e35c86812737c73f7e8a9dee
Thomas Muat <Thomas_Muat@hotmail.com>
2021-02-23 18:01:30 +0000

    Created tests for tokens

--- app/user/tests/test_user.py
+++ app/user/tests/test_user.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -68,3 +69,47 @@ class PublicUsersApiTest(TestCase):
             email=payload['email'],
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """
+        Test that a token is created for the user
+        """
+        payload = {
+            'email': 'test@test.com',
+            'password': 'testpass',
+            'name': 'Test Name'
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid(self):
+        """
+        Test that a token is created for the user.
+        """
+        create_user(email='test@test.com', password='testpass')
+        payload = {'email': 'test@test.com', 'password': 'wrong'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """
+        Test that token is not created if user does not exist.
+        """
+        payload = {'email': 'test@test.com', 'password': 'testpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """
+        Test that email and password are required.
+        """
+        res = self.client.post(TOKEN_URL, {'email': 'test@test.com', 'password': ''})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
